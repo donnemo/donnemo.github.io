14967a6a0a7574c7804266bdd5e06619e132e6b7
Andrew-APUS <32301528+Andrew-APUS@users.noreply.github.com>
2024-12-16 13:40:13 -0500

    Initial Publish

--- andrew-social.php
+++
@@ -1,5 +0,0 @@
-<p>
-    <span><a href="https://instagram.com/andyandporky" target="_blank"><i class="fa fa-instagram"></i></a></span>
-    <span><a href="https://github.com/donnemo/" target="_blank"><i class="fa fa-github"></i></a></span>
-    <span><a href="https://codepen.io/apaulson" target="_blank"><i class="fa fa-codepen"></i></a></span>
-</p>
